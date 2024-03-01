1b670835edfb863aa8a1eab36727dc44280ec192
MaryyBo <marusya.bodrug@gmail.com>
2024-03-01 18:59:22 +0100

    State

--- index.js
+++ index.js
@@ -1,50 +1,4 @@
 
-
-class HeaderComponent extends React.Component { // клас це компонент
-    render() {//єдиний метод який ми зобов'язані визначити в класі
-
-        console.log(this.props);
-        const p = React.createElement('p', {}, 'Super paragraph');
-        const h2 = React.createElement('h2', {title: 'Hi'},`Hello ${this.props.name}`); // Title підказка, яка вилазить при наведенні 
-        const article = React.createElement('article', {}, h2, p);
-        return article; //метод render  - це ОБОВʼЯЗКОВО!!! прописуємо те, яку верстку поверне нам компонента
-    }
-}
-
-const component = React.createElement(HeaderComponent, {name:'John'}, 'Text---> John');
-const component2 = React.createElement(HeaderComponent, {name:'Oleh'},  'Text---> Oleh');
-
-const parentElement = React.createElement('section',{},component,component2);
-
-
-const root = document.querySelector('#root');
-
-ReactDOM.render(parentElement, root);
-
-
-//Props вхідні дані React компонентів що передаються від батьківських до дочірніх компонентів
-// Propsам можемо щось передавати ззовні.
-
-
-/*Зробіть класову компоненту,
- яка приймає через props ім'я людини і повертає h1 
- з текстом "Привіт, ім'я_яке_вам_передали"*/
-
-// class HelloComp extends React.Component {
-//  render() {
-//     const h1 = React.createElement('h1', {},`Hello ${this.props.name}`);
-//     return h1;
-//  }
-// }
-
-// const hello = React.createElement(HelloComp, {name:'Maryna'});
-
-// ReactDOM.render(hello, root);
-
-// const root = document.querySelector('#root');
-
-
-
 /*
 1. REACT - бібліотека для створення користувацьких інтерфейсів
 2. Можна одночасно використовувати як звичайний ванільний JS, так само як і REACT
@@ -54,4 +8,38 @@ ReactDOM.render(parentElement, root);
 6. REACT оперує REACT-елементами
 7. REACT-елементами - це об'єкт. Він набагато легший ніж той самий елемент у нативному DOMі
 8. З цих REACT-елементів, як маленьких блоків складається REACT-компоненти, а з REACT-компонентів складаються сторінки, які будуть бачити наші користувачі
+9. State - Стан компонента , це такі дані, які можуть змнінюватись за час життя нашої компоненти
+State ---> тільки в конструкторі!
 */
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props); //передати props батьківському класу React.Component 
+        this.state = { // state - це стан нашого додатку
+            count: 0
+        }
+    }
+
+    increment() {
+        //to do: state.count++;
+        // console.log('click');// щоб побачити в консолі, перевірка
+
+       this.setState({ //треба взяти функцію з React.Component
+        count: this.state.count + 1
+       }) 
+
+    }
+
+    render() {
+        const h2 = React.createElement('h2', {}, this.state.count);
+        const button = React.createElement('button', {onClick: () => {this.increment()}}, '+'); // в якості this тут Counter, тому тут завжди буде СТРІЛКОВА ФУНКЦІЯ
+        return React.createElement(React.Fragment, {}, h2, button);;
+    }
+
+}
+
+const root = document.querySelector('#root');// знайти в html root
+const component = React.createElement(Counter);
+
+ReactDOM.render(component, root);
+
